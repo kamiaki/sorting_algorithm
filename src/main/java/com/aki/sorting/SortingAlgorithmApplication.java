44f6c8121237244f75caa63e1dce69733da0db4b
170725e <247557768@qq.com>
2018-10-15 11:01:51 +0800

    排序算法

---
+++ src/main/java/com/aki/sorting/SortingAlgorithmApplication.java
@@ -0,0 +1,12 @@
+package com.aki.sorting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortingAlgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortingAlgorithmApplication.class, args);
+	}
+}
---
+++ src/test/java/com/aki/sorting/SortingAlgorithmApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aki.sorting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SortingAlgorithmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
