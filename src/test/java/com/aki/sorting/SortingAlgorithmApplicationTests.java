12b30278312cf54797a466c83b9e61dfe279b306
170725e <247557768@qq.com>
2018-10-15 16:37:47 +0800

    冒泡写好了

--- src/test/java/com/aki/sorting/SortingAlgorithmApplicationTests.java
+++ src/test/java/com/aki/sorting/SortingAlgorithmApplicationTests.java
@@ -1,16 +1,44 @@
 package com.aki.sorting;
 
+import com.aki.sorting.service.ServiceSorting;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import org.json.JSONArray;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SortingAlgorithmApplicationTests {
+	@Autowired
+	@Qualifier(value = "sort")
+	ServiceSorting serviceSorting;
+
+	private int[] ints;
+	@Before
+	public void first(){
+		ints = new int[10];
+		for (int i = 0; i < 10; i++) {
+			ints[i] = Long.valueOf(Math.round(Math.random()*(100-0)+0)).intValue();
+		}
+	}
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads()throws Exception {
+		Gson gson = new Gson();
+		System.out.println(gson.toJson(ints));
+		serviceSorting.bubblesort(ints);
+		System.out.println(gson.toJson(ints));
 	}
 
 }
